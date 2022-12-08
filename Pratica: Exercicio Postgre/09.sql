1a704c033083b2fa244a613ef596bdcccfc0cb63
Diego Candido <diegocan.sou@gmail.com>
2022-12-07 21:09:51 -0300

    fix: 9, 10 and 11

--- Pratica: Exercicio Postgre/09.sql
+++ Pratica: Exercicio Postgre/09.sql
@@ -1,1 +1,3 @@
+SELECT (id) FROM clientes WHERE nome = 'Chico Buarque de Holanda';
+SELECT (id) FROM produto WHERE nome = 'Violão Lava ME 2';
 INSERT INTO compras (id_cliente, id_produto) VALUES (8,10);
--- Pratica: Exercicio Postgre/10.sql
+++ Pratica: Exercicio Postgre/10.sql
@@ -1,1 +1,4 @@
+SELECT (id) FROM clientes WHERE nome = 'Olga Cascais Fortunato';
+SELECT (id) FROM produto WHERE nome = 'Celular Topo de Linha';
+INSERT INTO compras (id_cliente, id_produto) VALUES (4, 5);
 INSERT INTO compras (id_cliente, id_produto) VALUES (4, 5);
